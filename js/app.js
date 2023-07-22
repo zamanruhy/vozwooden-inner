70c8acf77ece357828a782b99cb5dfdefa0b57b5
Ruslan Shakirov <ruslanshakir@gmail.com>
2023-07-22 11:21:50 +0300

    deploy

--- js/app.js
+++ js/app.js
@@ -13,3 +13,17 @@ const SiteAbout = "";
 const SiteCheckbox = "";
 const SiteInput = "";
 const SiteFeedback = "";
+(function() {
+  var $ = window.jQuery;
+  var $search = $(".site-inner-search");
+  var $trigger = $(".site-inner-search__trigger", $search);
+  var $close = $(".site-inner-search__close", $search);
+  var $input = $(".site-inner-search__input", $search);
+  $trigger.on("click", function(e) {
+    $search.addClass("site-inner-search_open");
+    $input.focus();
+  });
+  $close.on("click", function(e) {
+    $search.removeClass("site-inner-search_open");
+  });
+})();
